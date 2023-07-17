85c8035fbfb1a475a2c3564b8748c4eb109e44a7
elyette <elyette.gandji@gmail.com>
2023-07-17 13:01:10 +0200

    création db avec sqitch

--- README.md
+++ README.md
@@ -1,42 +1,24 @@
 # Devsconnect
 O'clock TP project
 
-## Stack technique
- "dependencies": {
-    "bcrypt": "^5.1.0",
-    "dotenv": "^16.0.3",
-    "ejs": "^3.1.8",
-    "email-validator": "^2.0.4",
-    "express": "^4.18.2",
-    "express-session": "^1.17.3",
-    "pg": "^8.10.0",
-    "pg-hstore": "^2.3.4",
-    "sequelize": "^6.29.2"
-  },
-  "devDependencies": {
-    "eslint": "^8.35.0",
-    "nodemon": "^2.0.21"
-  }
-
-### Backend
+## Backend
 
 * Node.js (runtime)
-* express (framework) -
-* joi -
-* jsonwebtoken -
-* sequelize (ORM) -
-* postgres (database) -
-* dotenv (env variables) -
-* eslint -
-* nodemon -
-* body-parser -
-* faker -
-* swagger-jsdoc -
-* swagger-ui-express -
-* bcrypt -
-* bunyan -
-* cors -
-* debug -
+* express (framework)
+* joi
+* jsonwebtoken
+* postgres (database)
+* dotenv (env variables)
+* eslint
+* nodemon
+* body-parser
+* faker
+* swagger-jsdoc
+* swagger-ui-express
+* bcrypt
+* bunyan
+* cors
+* debug
 * sqitch
 
 ### Frontend
---
+++ back/app/validations/userSchema.js
@@ -0,0 +1,44 @@
+const Joi = require('joi');
+
+const userCreate = Joi.object({
+  name: Joi.string()
+    .max(30)
+    .required(),
+  firstname: Joi.string()
+    .max(30)
+    .required(),
+  pseudo: Joi.string()
+    .max(30)
+    .required(),
+  email: Joi.string()
+    .email({minDomainSegments: 2})
+    .required(),
+  password: Joi.string()
+  // regex exemple not implemented for demo "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+    .pattern(new RegExp())
+    .required(),
+  description: Joi.string().allow('').allow(null),
+  availability: Joi.boolean(),
+  tags: Joi.array()
+    .items(Joi.number().integer()),
+});
+
+const userUpdate = Joi.object({
+  name: Joi.string()
+    .max(30),
+  firstname: Joi.string()
+    .max(30),
+  pseudo: Joi.string()
+    .max(30),
+  email: Joi.string()
+    .email({minDomainSegments: 2}),
+  password: Joi.string().allow('').allow(null)
+    // regex exemple not implemented for demo "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+    .pattern(new RegExp()),
+  description: Joi.string().allow('').allow(null),
+  availability: Joi.boolean(),
+  tags: Joi.array()
+    .items(Joi.number().integer()),
+});
+
+module.exports = { userCreate, userUpdate };
